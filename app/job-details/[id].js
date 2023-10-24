6f1bed552a36ad4c8f4ab8c318f8b1edc55310ca
yawai990 <ywaung990@gmail.com>
2023-10-24 17:30:41 +0630

    job details page updated

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -27,6 +27,10 @@ const JobDetails = () => {
     job_id: params.id,
   });
 
+  const [refreshing, setRereshing] = useState(false);
+
+  const onRefresh = () => {};
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -46,7 +50,33 @@ const JobDetails = () => {
           ),
           headerTitle: "",
         }}
-      ></Stack.Screen>
+      />
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size={"large"} color={COLORS.primary} />
+          ) : isError ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No Data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs />
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
   );
 };
